02daba62d4bca37f05cf4f15ff77aa9648775a04
DorisMora09 <105517040+DorisMora09@users.noreply.github.com>
2024-09-21 20:10:52 -0500

    añadir estilo css

--- login.php
+++ login.php
@@ -34,7 +34,8 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 <html lang="en">
 <head>
   <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+  <link rel="stylesheet" href="estilo.css"> 
   <title>Iniciar Sesion</title>
 </head>
 <body>
